21441a32fac44ad684f2106b368fd1194116423b
coji <coji@techtalk.jp>
2024-03-11 17:41:41 +0900

    Add toast notifications and loader function to root component

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,23 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare'
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
+import { useEffect } from 'react'
+import { getToast } from 'remix-toast'
+import { toast } from 'sonner'
+import { Toaster } from '~/components/ui'
 import './tailwind.css'
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { toast: toastData, headers } = await getToast(request)
+  return json({ toastData }, { headers: toastData ? headers : undefined })
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -16,6 +28,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Toaster />
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -25,5 +38,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+  const { toastData } = useLoaderData<typeof loader>()
+
+  useEffect(() => {
+    if (!toastData) return
+
+    let toastFn = toast.info
+    if (toastData.type === 'success') {
+      toastFn = toast.success
+    } else if (toastData.type === 'error') {
+      toastFn = toast.error
+    }
+    toastFn(toastData.message, {
+      description: toastData.description,
+    })
+  }, [toastData])
+
   return <Outlet />
 }
