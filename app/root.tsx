c46ae738b1f838b1639a5ea5847e5b1a3cbcf917
coji <coji@techtalk.jp>
2024-03-11 15:13:37 +0900

    chore: add tailwindcss, prettier, biomejs

--- app/root.tsx
+++ app/root.tsx
@@ -4,8 +4,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-
+} from '@remix-run/react'
+import './tailwind.css'
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -21,9 +21,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,20 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: 'New Remix App' },
     {
-      name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
+      name: 'description',
+      content: 'Welcome to Remix! Using Vite and Cloudflare!',
     },
-  ];
-};
+  ]
+}
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
+    <div className="mx-4 my-2 font-[system-ui,sans-serif] leading-8">
+      <h1 className="text-2xl">Welcome to Remix (with Vite and Cloudflare)</h1>
+      <ul className="list-inside list-disc">
         <li>
           <a
             target="_blank"
@@ -31,5 +31,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
